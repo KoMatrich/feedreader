4db5ac020816088cbf2014b017e2456376479d41
KoMatrich <kocichmartin@gmail.com>
2022-10-10 12:08:24 +0200

    link file reader done

--- include/util.h
+++ include/util.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 enum PrintMode
 {
 	Debug,
@@ -23,3 +25,14 @@ inline const char* Bool2String(bool b) {
 	return b ? "true" : "false";
 }
 
+/// <summary>
+/// trims whitespaces
+/// </summary>
+inline std::string trim(const std::string& str)
+{
+	const char* whitespace = " \t\n\r";
+	size_t first = str.find_first_not_of(whitespace);
+	if (first == std::string::npos) first = 0;
+	size_t last = str.find_last_not_of(whitespace);
+	return str.substr(first, (last - first + 1));
+}
--- src/feedreader.cpp
+++ src/feedreader.cpp
@@ -1,50 +1,68 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 #include <openssl/ssl.h>
+#include <iostream>
+#include <util.h>
+#include <vector>
+#include <fstream>
 
 #include "inputParser.h"
 #include "parsedLink.h"
 #include "webScraper.h"
-#include <iostream>
-#include <util.h>
-#include <vector>
 
 int main(int argc, char* argv[])
 {
-    optionParser oParser;
-    oParser.run(argc, argv);
+	optionParser oParser;
+	oParser.run(argc, argv);
 
 	//get certificates
-	if(OPENSSL_init_ssl(OPENSSL_INIT_SSL_DEFAULT, nullptr)<0)
+	if (OPENSSL_init_ssl(OPENSSL_INIT_SSL_DEFAULT, nullptr) < 0)
 	{
 		Logger::Print(RError, "OPENSSL_init_ssl failed");
-		exit(EXIT_FAILURE);
+		return(EXIT_FAILURE);
 	}
-	
+
 	Logger::Print(Debug, "%s", OpenSSL_version(OPENSSL_VERSION));
-	
+
 	//inicialize web scraper
-	WebScraper WScraper(oParser.certAddr,oParser.certFile);
-	
+	WebScraper WScraper(oParser.certAddr, oParser.certFile);
+
 	vector<string> links;
-	
+
 	//get links from file
 	if (oParser.WFlag) {
+		Logger::Print(Runtime, "");
 		links.push_back(oParser.feedPath.data());
-	}else {
-		
 	}
-	
+	else {
+		const char* fileName = oParser.feedPath.data();
+		std::ifstream file{ fileName };
+		if (!file.is_open()) {
+			Logger::Print(RError, "Error opening file '%s'", fileName);
+			return(EXIT_FAILURE);
+		}
+		
+		string line;
+		while (std::getline(file, line)) {
+			line = trim(line);
+			if (line.empty()) continue;
+			if (line.at(0) == '#') continue;
+			links.push_back(line);
+		}
+		file.close();
+	}
+
 	//run web scraper
 	do {
 		string SLink = links.back();
 		links.pop_back();
-		
+
 		ParsedLink link{ SLink };
-		int return_code = WScraper.run(link,3,3);
-		std::cout << WScraper.response.raw();
+		int return_code = WScraper.run(link, 3, 3);
+		std::ofstream outfile{ "outfile.txt" };
+		outfile << WScraper.response.raw();
 		if (return_code != WebScraper::Error::OK) continue;
 	} while (!links.empty());
-	
-    exit(EXIT_SUCCESS);
+
+	return(EXIT_SUCCESS);
 }
